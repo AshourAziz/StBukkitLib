c21aeeff204e4931c69563ba2b2ce77ee5b6bc74
Stealth2800 <stealth2800@stealthyone.com>
2014-05-24 23:15:21 -0700

    Fixed divide by 0 exception

--- src/main/java/com/stealthyone/mcb/stbukkitlib/utils/MiscUtils.java
+++ src/main/java/com/stealthyone/mcb/stbukkitlib/utils/MiscUtils.java
@@ -5,6 +5,10 @@ public final class MiscUtils {
     private MiscUtils() { }
 
     public static int getPageCount(int itemCount, int amtPerPage) {
+        if (itemCount == 0 || amtPerPage == 0) {
+            return 0;
+        }
+
         int pageCount = 0;
         for (int i = 1; i <= itemCount; i++) {
             if (i % amtPerPage == 1) pageCount++;
