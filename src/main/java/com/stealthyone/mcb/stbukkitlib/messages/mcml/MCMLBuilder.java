8e8193ec8027e327f4e8e231984836f7d417a19b
Stealth2800 <stealth2800@stealthyone.com>
2014-06-24 00:33:24 -0700

    MCML is now its own project, not going to be included in StBukkitLib officially.

--- src/main/java/com/stealthyone/mcb/stbukkitlib/messages/mcml/MCMLBuilder.java
+++ src/main/java/com/stealthyone/mcb/stbukkitlib/messages/mcml/MCMLBuilder.java
@@ -1,5 +1,25 @@
+/*
+ * Minecraft Markup Language (MCML)
+ * Copyright (C) 2014 Stealth2800 <stealth2800@stealthyone.com>
+ * Website: <http://stealthyone.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stealthyone.mcb.stbukkitlib.messages.mcml;
 
+import com.stealthyone.mcb.stbukkitlib.messages.MCMLClickEvent;
+import com.stealthyone.mcb.stbukkitlib.messages.MCMLHoverEvent;
 import mkremins.fanciful.FancyMessage;
 import org.apache.commons.lang.Validate;
 import org.bukkit.ChatColor;
@@ -24,6 +44,7 @@ public class MCMLBuilder {
         Validate.notNull(inputText, "Input text cannot be null.");
         Validate.notNull(replacements, "Replacements cannot be null.");
         Validate.noNullElements(replacements.keySet(), "Replacements map cannot contain null keys.");
+        this.replacements = replacements;
 
         char[] inputChars = inputText.toCharArray();
 
