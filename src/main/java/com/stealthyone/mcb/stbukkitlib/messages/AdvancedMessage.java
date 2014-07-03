20d1968b18c7b601f692af83b5c0e6c36494bbab
Stealth2800 <stealth2800@stealthyone.com>
2014-07-02 18:55:25 -0700

    Added new messaging system that uses MCML.

---
+++ src/main/java/com/stealthyone/mcb/stbukkitlib/messages/AdvancedMessage.java
@@ -0,0 +1,118 @@
+package com.stealthyone.mcb.stbukkitlib.messages;
+
+import com.stealthyone.mcb.mcml.MCMLBuilder;
+import mkremins.fanciful.FancyMessage;
+import org.apache.commons.lang.Validate;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class AdvancedMessage {
+
+    private AdvancedMessageManager messageManager;
+    private String category;
+    private List<String> messages = new ArrayList<>();
+
+    AdvancedMessage(AdvancedMessageManager messageManager, ConfigurationSection section, String name) {
+        this.messageManager = messageManager;
+        if (this instanceof AdvancedDummyMessage) return;
+
+        category = section.getName();
+        Object obj = section.get(name);
+        if (obj instanceof String) {
+            messages.add((String) obj);
+        } else if (obj instanceof List) {
+            messages.addAll((List) obj);
+        } else {
+            throw new IllegalArgumentException("Invalid message value for '" + section.getName() + "." + name + "' in messages.yml for plugin: " + messageManager.getOwner().getName());
+        }
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * Returns all the raw string messages defined for the message object.
+     *
+     * @return A list of the messages.
+     */
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    /**
+     * Returns the messages that would be sent to a player.
+     *
+     * @return An array of the formatted messages.
+     */
+    public List<FancyMessage> getFormattedMessages() {
+        List<FancyMessage> returnList = new ArrayList<>();
+        for (String msg : messages) {
+            if (msg == null || msg.equals("")) continue;
+            String finalMsg = msg;
+            if (finalMsg.contains("{TAG}")) {
+                finalMsg = finalMsg.replace("{TAG}", messageManager.getTag(this));
+            }
+            returnList.add(new MCMLBuilder(msg).buildFancyMessage());
+        }
+        return returnList;
+    }
+
+    /**
+     * Returns the messages that would be sent to a player.
+     *
+     * @return An array of the formatted messages with replacements.
+     */
+    public List<FancyMessage> getFormattedMessages(Map<String, String> replacements) {
+        List<FancyMessage> returnList = new ArrayList<>();
+        for (String msg : messages) {
+            if (msg == null || msg.equals("")) continue;
+            String finalMsg = msg;
+            if (finalMsg.contains("{TAG}")) {
+                finalMsg = finalMsg.replace("{TAG}", messageManager.getTag(this));
+            }
+            for (Entry<String, String> replacement : replacements.entrySet()) {
+                if (!finalMsg.contains(replacement.getKey())) continue;
+                finalMsg = finalMsg.replace(replacement.getKey(), replacement.getValue());
+            }
+            returnList.add(new MCMLBuilder(finalMsg).buildFancyMessage());
+        }
+        return returnList;
+    }
+
+    /**
+     * Sends the message to a CommandSender.
+     *
+     * @param sender CommandSender to send all messages to.
+     */
+    public void sendTo(CommandSender sender) {
+        Validate.notNull(sender, "Sender must not be null.");
+
+        for (FancyMessage message : getFormattedMessages()) {
+            message.send(sender);
+        }
+    }
+
+    /**
+     * Sends the message to a CommandSender with a list of replacements.
+     *
+     * @param sender CommandSender to send all messages in the string list to.
+     * @param replacements Replacements to use in the sent message(s).
+     *                     EX. "{SENDER}", "Stealth2800"
+     */
+    public void sendTo(CommandSender sender, Map<String, String> replacements) {
+        Validate.notNull(sender, "Sender must not be null.");
+        Validate.notNull(sender, "Replacements must not be null.");
+        Validate.notEmpty(replacements, "Replacements must not be empty.");
+
+        for (FancyMessage message : getFormattedMessages(replacements)) {
+            message.send(sender);
+        }
+    }
+
+}
---
+++ src/main/java/com/stealthyone/mcb/stbukkitlib/messages/AdvancedMessageManager.java
@@ -0,0 +1,128 @@
+package com.stealthyone.mcb.stbukkitlib.messages;
+
+import com.stealthyone.mcb.stbukkitlib.logging.LogHelper;
+import com.stealthyone.mcb.stbukkitlib.storage.YamlFileManager;
+import org.apache.commons.lang.Validate;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * An upgraded version of {@link com.stealthyone.mcb.stbukkitlib.messages.MessageManager} that uses <a href="https://github.com/Stealth2800/MCMarkupLanguage">MCML</a> for
+ * more advanced messages.
+ */
+public class AdvancedMessageManager {
+
+    private JavaPlugin owner;
+
+    /* The file containing the messages */
+    private YamlFileManager messageFile;
+
+    /* Message returned for the getMessage methods when the path is invalid */
+    private AdvancedMessage unknownMessage;
+
+    /* A list of all loaded messages. Format: <Category, <Name, Message object>> */
+    private Map<String, Map<String, AdvancedMessage>> loadedMessages = new HashMap<>();
+
+    private String defaultTag = "&6[{PLUGIN}] &r";
+
+    /* A list of all message tags. Format: <Category, Tag> */
+    private Map<String, String> tags = new HashMap<>();
+
+    /**
+     * Creates a new instance of the message manager.
+     * Uses a file called 'messages.yml' located in the plugin's base directory.
+     *
+     * @param plugin The plugin that owns the message manager.
+     */
+    public AdvancedMessageManager(JavaPlugin plugin) {
+        this(plugin, plugin.getDataFolder() + File.separator + "messages.yml");
+    }
+
+    /**
+     * Creates a new instance of the message manager.
+     *
+     * @param plugin The plugin that owns the message manager.
+     * @param filePath The path of the file containing the messages.
+     */
+    public AdvancedMessageManager(JavaPlugin plugin, String filePath) {
+        Validate.notNull(plugin, "Plugin cannot be null.");
+        Validate.notNull(filePath, "File path cannot be null.");
+
+        this.owner = plugin;
+        this.messageFile = new YamlFileManager(filePath);
+        unknownMessage = new AdvancedDummyMessage("&cUnknown message: &4{PATH}&c.");
+    }
+
+    public JavaPlugin getOwner() {
+        return owner;
+    }
+
+    /**
+     * Reloads all messages from the messageFile.
+     */
+    public void reload() {
+        tags.clear();
+        loadedMessages.clear();
+
+        FileConfiguration config = messageFile.getConfig();
+
+        for (String category : config.getKeys(false)) {
+            if (category.equals("tag")) {
+                defaultTag = category;
+                continue;
+            }
+
+            Map<String, AdvancedMessage> messages = new HashMap<>();
+
+            //Load each individual message in the category.
+            for (String msgName : config.getConfigurationSection(category).getKeys(false)) {
+                if (msgName.equals("tag")) {
+                    tags.put(category, config.getString(category + "." + msgName));
+                    continue;
+                }
+
+                AdvancedMessage newMsg;
+                try {
+                    newMsg = new AdvancedMessage(this, config.getConfigurationSection(category), msgName);
+                } catch (Exception ex) {
+                    //An error occurred when trying to create the message object, log and continue
+                    LogHelper.warning(owner, "An error occurred while trying to load message '" + msgName + "' in category '" + category + "' in messages.yml for " + owner.getName());
+                    ex.printStackTrace();
+                    continue;
+                }
+                messages.put(msgName.toLowerCase(), newMsg);
+            }
+            loadedMessages.put(category.toLowerCase(), messages);
+        }
+    }
+
+    String getTag(AdvancedMessage message) {
+        return getTag(message.getCategory());
+    }
+
+    String getTag(String category) {
+        String tag = tags.get(category);
+        if (tag == null) tag = defaultTag;
+        return tag.replace("{PLUGIN}", owner.getName());
+    }
+
+    public AdvancedMessage getMessage(String path) {
+        String[] pathSplit = path.split("\\.");
+        if (pathSplit.length != 2) {
+            throw new IllegalArgumentException("Invalid message path format '" + path + "'");
+        }
+
+        AdvancedMessage message;
+        try {
+            message = loadedMessages.get(pathSplit[0]).get(pathSplit[1]);
+        } catch (Exception ex) {
+            message = null;
+        }
+        return message == null ? unknownMessage : message;
+    }
+
+}
